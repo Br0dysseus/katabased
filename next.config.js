b5507ed34df6e2ff4e6a52e7740ee0a801051d30
Your Name <your-email@example.com>
2026-04-08 22:05:13 -0400

    fix: alias react-native async storage to false for browser builds

--- next.config.js
+++ next.config.js
@@ -2,7 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   webpack: (config) => {
-    config.externals = [...(Array.isArray(config.externals) ? config.externals : []), 'pino-pretty', 'lokijs', 'encoding', '@react-native-async-storage/async-storage'];
+    config.externals = [...(Array.isArray(config.externals) ? config.externals : []), 'pino-pretty', 'lokijs', 'encoding'];
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@react-native-async-storage/async-storage': false,
+    };
     return config;
   },
 };
